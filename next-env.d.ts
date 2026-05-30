590aee75e0b7ae971cfe519be420061d484b5d32
abubakar_dev <abubakar.suleiman@gmail.com>
2026-05-30 07:46:27 +0100

    build: adjust configuration file next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/// <reference path="./.next/types/routes.d.ts" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+
+// Contribution check by alexdev99 at 2025-01-22T20:33:06
+
+// Contribution check by lisap at 2025-04-29T02:04:08
+
+// Contribution check by karen-s at 2025-08-03T07:35:10
+
+// Contribution check by alexdev99 at 2025-11-07T13:06:12
+
+// Contribution check by lisap at 2026-02-11T18:37:14
+
+// Contribution check by karen-s at 2026-05-19T00:08:16
